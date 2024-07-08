14a01a15c4e9bd395af85e507d9d9b67a3b3dd6a
Park Ji Eun <mo111b@naver.com>
2024-07-08 11:05:24 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,12 +1,3 @@
 def solution(my_string):
     answer = 0
-
-    for i in my_string:
-        if i.isalpha(): # 문자라면
-            my_string = my_string.replace(i, ' ') # 공백으로 대체하기
-    my_string = my_string.split() # 공백 기준으로 분할해 리스트로 만들기
-    
-    # my_string 리스트의 요소를 int로 변환해 숫자들을 모두 더해줌
-    answer = sum(map(int, my_string))
-
     return answer
