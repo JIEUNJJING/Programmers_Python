cb949069b7d78980de7ec4ad97bbe293688ad362
Park Ji Eun <mo111b@naver.com>
2024-07-08 10:32:22 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    num = 1
+
+    for i in range(1, 11):
+        num *= i
+        if num == n:
+            answer = i
+        elif num <= n:
+            answer = i
+    return answer
