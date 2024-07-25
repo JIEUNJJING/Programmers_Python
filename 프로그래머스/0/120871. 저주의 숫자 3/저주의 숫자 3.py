eeb2b9c8341d4c8dae8c97b805428cc2933d1b22
Park Ji Eun <mo111b@naver.com>
2024-07-25 09:14:11 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    i = 1
+
+    while (n):
+        if i % 3 != 0 and '3' not in str(i):
+            n -= 1
+        i += 1
+        answer = i - 1
+    return answer
