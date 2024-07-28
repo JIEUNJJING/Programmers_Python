eba3cc42a466b10a16d46a89384348f2147641c3
Park Ji Eun <mo111b@naver.com>
2024-07-29 08:36:23 +0900

    [level 1] Title: 약수의 합, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    i = 1
+
+    while (i <= n):
+        if n % i == 0:
+            answer += i
+            i += 1
+        else:
+            i += 1
+            continue
+
+    return answer
