15618a1451169f144fbbf2c709cd2e17106ae874
Park Ji Eun <mo111b@naver.com>
2024-08-01 11:43:27 +0900

    [level 1] Title: 최소직사각형, Time: 7.76 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,10 @@
+def solution(sizes):
+    answer = 0
+    width = []
+    height = []
+
+    for i in sizes:
+        width.append(max(i))
+        height.append(min(i))
+    answer = max(width) * max(height)
+    return answer
