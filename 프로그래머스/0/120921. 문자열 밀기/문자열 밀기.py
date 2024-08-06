8fca66e74e50e52a4a18d1e7e301a699608dd9c3
Park Ji Eun <mo111b@naver.com>
2024-08-06 13:40:57 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,11 +1,14 @@
 def solution(A, B):
-    answer = -1
-    a_len = len(A)
+    answer = 0
 
-    for i in range(a_len): # A길이만큼 반복 
-        if(A == B): # A와 B가 같다면
-            answer = i # 첫번째 비교라 i에 0이 들어가 있음(0 반환)
+    if A == B:
+        return 0
+
+    for i in range(len(A)): # A길이만큼 반복 
+        if A == B:# A와 B가 같다면
+            answer = i # 1 반환
             break
-        A = A[-1] + A[0:a_len-1] # 젤 뒤에 문자를 젤 앞으로 보내줌
-
+        A = A[-1] + A[0:len(A) - 1] # A와 B가 다르면, 맨 뒤 문자를 제일 앞으로 옮겨 다시 비교
+    if A != B:
+        answer = -1
     return answer
